71a1a51b4d29429f6d046d8f50318ca09ed6ea37
lokanadham <lokanadham.o@hotmail.com>
2020-12-09 17:02:28 +0530

    added sample form

---
+++ src/components/SampleForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SampleForm() {
+    return (
+        <div>
+            <form>
+                <label htmlFor='name'>Name</label>
+                <input type= 'text' id='name' name='name' />
+                <label htmlFor='email'>E-mail</label>
+                <input type= 'email' id='email' name='email' />
+                <label htmlFor='channel'>Chanel</label>
+                <input type= 'text' id='channel' name='channel' />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SampleForm
